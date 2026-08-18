6425796584e17710671f1b28f402023154118901
icansoletmein <xuan8906@gmail.com>
2026-08-18 16:15:29 +0800

    Update inv.h

--- inv.h
+++ inv.h
@@ -4,6 +4,6 @@ class inv
 	static char invinside[17][35];
 public:
 	void OpenInv() const;
-	void RecivedInv(char thing) const;
+	void RecivedInv(char thing) const; //reciving something, checks if its full inside
 };
 
