0a6cf8f7f4a406f3c1190de7bfcee0e2dcab1daf
lucaducc <lucasleowxunci3@gmail.com>
2026-08-19 13:34:54 +0800

    Add files via upload

--- Entity.h
+++ Entity.h
@@ -2,30 +2,12 @@
 class Entity
 {
 public:
-	Entity(int hp, int meleeDef, int projectileDef);
-
+	Entity(int hp);
 	int gethp();
 	int getmaxhp();
-	
-	//new code (pharrell) getter 
-	int getMeleeDefense();
-	int getProjectileDefense();
-
 	bool isalive();
-	void takeDamage(int damage, bool isProjectile); //check for projectile or melee damage
-
-
-	//new code(pharrell) setters
-	void setMeleeDefense(int meleeDef);
-	void setProjectileDefense(int projectileDef);
-
-
 private:
 	int hp;
 	int maxhp;
-
-	//new code (pharrell)
-	int meleeDefense;
-	int projectileDefense;
 };
 
