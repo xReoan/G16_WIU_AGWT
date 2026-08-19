8b42e09c598dcd141ecc2c34d7542b3136c6bd57
pharrell138 <pharrelldanish0804@gmail.com>
2026-08-19 11:33:42 +0800

    updated entity header

--- Entity.h
+++ Entity.h
@@ -2,12 +2,30 @@
 class Entity
 {
 public:
-	Entity(int hp);
+	Entity(int hp, int meleeDef, int projectileDef);
+
 	int gethp();
 	int getmaxhp();
+	
+	//new code (pharrell) getter 
+	int getMeleeDefense();
+	int getProjectileDefense();
+
 	bool isalive();
+	void takeDamage(int damage, bool isProjectile); //check for projectile or melee damage
+
+
+	//new code(pharrell) setters
+	void setMeleeDefense(int meleeDef);
+	void setProjectileDefense(int projectileDef);
+
+
 private:
 	int hp;
 	int maxhp;
+
+	//new code (pharrell)
+	int meleeDefense;
+	int projectileDefense;
 };
 
