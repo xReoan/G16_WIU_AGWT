1c9a0fa4d6000c7600ed1c34678ef939624842b1
icansoletmein <xuan8906@gmail.com>
2026-08-20 10:35:19 +0800

    Update inv.cpp

--- inv.cpp
+++ inv.cpp
@@ -15,7 +15,7 @@ void inv::OpenInv() const
 {
     for (int i = 0; i < 16; i++) {
         if (inv::invinside[i] != -1 ) {
-            std::cout << i + 1 << ". " << invitem[inv::invinside[i]]->getname() << std::endl;
+            std::cout << i + 1<<". " << invitem[inv::invinside[i]]->getname() << std::endl;
         }
         else {
             std::cout << i + 1 << ". Empty" << std::endl;
@@ -28,7 +28,7 @@ void inv::RecivedInv(int thing) const
 {
     char changed = 'N';
     for (int i = 0; i < 16; i++) {
-        if (invinside[i] == '?') {
+        if (invinside[i] == -1) {
             invinside[i] = thing;
             changed = 'Y';
             return;
@@ -36,5 +36,17 @@ void inv::RecivedInv(int thing) const
     }
     if (changed != 'Y') {
         std::cout << "Invantory full, cant store" << std::endl;
+    }
+}
+
+//like which of the 16 items slot is the item they used is from, can be changed
+void inv::UsedInv(int itemSlot) const
+{
+    if (invinside[itemSlot] != -1) {
+        std::cout << "Used " << invitem[invinside[itemSlot]]->getname() << std::endl;;
+        invinside[itemSlot] = -1;
+    }
+    else {//if it is emepty
+        std::cout << "no item to use"<<std::endl;
     }
 }
