11a8f69eecabcf1c9bbdf0f58adb390761594732
icansoletmein <xuan8906@gmail.com>
2026-08-20 09:56:52 +0800

    Update inv.h

--- inv.h
+++ inv.h
@@ -1,9 +1,16 @@
 #pragma once
+#include "itemDatabase.h"
 class inv
 {
-	static char invinside[17][35];
+private:
+	static int invinside[16];
+	item* invitem[10];
+	itemDatabase database;
+
 public:
+	inv();
 	void OpenInv() const;
-	void RecivedInv(char thing) const; //reciving something, checks if its full inside
+	void RecivedInv(int thing) const; //reciving something, checks if its full inside
+	
 };
 
